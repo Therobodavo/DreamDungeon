b2206e02acefc002a0d6a28a92518a6a7cd4e1ad
David Knolls <Therobodavo@gmail.com>
2019-04-28 12:30:01 -0400

    new weapon

---
+++ Assets/Scripts/Items/Weapon3.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon3 : WeaponBase
+{
+    RaycastHit hit;
+    Ray attackLine;
+    GameObject player;
+
+    public Weapon3(GameObject weapon)
+    {
+        
+    }
+
+    public override void UseItem()
+    {
+        attackLine = new Ray();
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        
+    }
+
+    public override void OnSelect()
+    {
+        
+    }
+
+    public override void OffSelect()
+    {
+        
+    }
+}
