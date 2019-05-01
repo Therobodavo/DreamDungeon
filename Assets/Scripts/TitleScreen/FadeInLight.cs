e94d021169394f50eb1810fae0576f67775c72cf
Joe Aquiare <brainfoammgmt@gmail.com>
2019-05-01 17:16:33 -0400

    Updated branch.

---
+++ Assets/Scripts/TitleScreen/FadeInLight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInLight : MonoBehaviour
+{
+    public float speed;
+    private float intensity;
+
+    void Start()
+    {
+        intensity = this.GetComponent<Light>().intensity;
+        this.GetComponent<Light>().intensity = 0;
+    }
+
+    void Update()
+    {
+        this.GetComponent<Light>().intensity = Mathf.Lerp(this.GetComponent<Light>().intensity, intensity, speed * Time.deltaTime);
+    }
+
+    
+}
---
+++ Assets/Scripts/UI/RotateAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAnimation : MonoBehaviour
+{
+    public float offset;         //The offset of the animation
+    public float movementFreq; //The movement frequency
+    public float movementAmp;  //The movmeent amplitude
+    public float rotationFreq; //The rotation frequency
+    public float rotationAmp;  //The rotation amplitude
+
+    private RectTransform t;   //The transform of the UI element
+    private float timer;       //The timer keeping track of time since instantiation
+     
+    /// <summary>
+    /// Creates the transform reference.
+    /// </summary>
+    void Start()
+    {
+        t = this.GetComponent<RectTransform>();
+    }
+
+    /// <summary>
+    /// Updates the transform based on time.
+    /// </summary>
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(t != null) //If the transform isn't null...
+        {
+            float positionChange = Mathf.Sin(timer * movementFreq + offset) * movementAmp; //Move accordingly
+            float rotationChange = Mathf.Sin(timer * rotationFreq + offset) * rotationAmp;
+            t.position += new Vector3(0, positionChange, 0);
+            t.Rotate(new Vector3(0, 0, rotationChange));
+        }
+    }
+}
